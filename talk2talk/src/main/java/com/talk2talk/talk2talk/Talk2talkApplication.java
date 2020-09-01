ed25584ab947991484ba7d69ee95de39b12b7d67
Patrick5455 <patrickojunde@gmail.com>
2020-09-01 11:11:00 +0100

    Created Springboot project

---
+++ talk2talk/src/main/java/com/talk2talk/talk2talk/Talk2talkApplication.java
@@ -0,0 +1,13 @@
+package com.talk2talk.talk2talk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Talk2talkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Talk2talkApplication.class, args);
+	}
+
+}
---
+++ talk2talk/src/test/java/com/talk2talk/talk2talk/Talk2talkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talk2talk.talk2talk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Talk2talkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
